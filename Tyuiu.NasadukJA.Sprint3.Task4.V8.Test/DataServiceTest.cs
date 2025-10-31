ebeae886f37d52f0bdaba7fac79e22cbbc0fea17
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-10-31 19:31:14 +0500

    Task4

--- Tyuiu.NasadukJA.Sprint3.Task4.V8.Test/DataServiceTest.cs
+++ Tyuiu.NasadukJA.Sprint3.Task4.V8.Test/DataServiceTest.cs
@@ -1,11 +1,22 @@
-﻿namespace Tyuiu.NasadukJA.Sprint3.Task4.V8.Test
+﻿using Tyuiu.NasadukJA.Sprint3.Task4.V8.Lib;
+
+namespace Tyuiu.NasadukJA.Sprint3.Task4.V8.Test
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 2237.227; 
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.NasadukJA.Sprint3.Task4.V8/Program.cs
+++ Tyuiu.NasadukJA.Sprint3.Task4.V8/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint3.Task4.V8.Lib;
+
+// See https://aka.ms/new-console-template for more information
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Использование операторов continue и break в цикле                 *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #8                                                              *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+Console.WriteLine("* функции y=(x/sin(x))+0.5. При х = 0 прервать цикл.                      *");
+Console.WriteLine("* Полученные значения перемножать.                                        *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага: " + startValue);
+Console.WriteLine("Конец шага: " + stopValue);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double result = ds.Calculate(startValue, stopValue);
+Console.WriteLine("Произведение значений функции = " + result);
+
+Console.ReadKey();
+
