0fac0a1f71a3155895dca4c18eb8bddb8c65bdd3
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-10-31 23:21:38 +0500

    Task7

--- Tyuiu.NasadukJA.Sprint3.Task7.V4.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint3.Task7.V4.Lib/DataService.cs
@@ -1,7 +1,33 @@
-﻿namespace Tyuiu.NasadukJA.Sprint3.Task7.V4.Lib
-{
-    public class DataService
-    {
+﻿using System.Security.Cryptography.X509Certificates;
+using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.NasadukJA.Sprint3.Task7.V4.Lib
+{
+    public class DataService : ISprint3Task7V4
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = Math.Cos(x) + x;
+
+                if (denominator == 0)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = Math.Round((((2.0 * x + 6.0) / (Math.Cos(x) + x)) - 3.0), 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+            }
+            return valueArray;
+        }
     }
 }
